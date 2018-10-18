6b637bde387e5c3a2a22b747aec4d1202f8246a1
Burk Friel <burk.friel.2022@student.lhschools.org>
2018-10-18 14:16:55 -0400

    start

---
+++ src/RockPaperSicissors.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class RockPaperSicissors {
+
+    public static void main(String[] largs) {
+        Scanner keyboard;
+
+        int playerpicked;
+        int computerpicked;
+        int computerscore;
+        int playerscore;
+        computerscore = 0;
+        playerscore = 0;
+
+        keyboard = new Scanner(System.in);
+
+        do {
+            
+
+
+
+
+        }while();
+
+
+
+
+
+    }
+
+}
